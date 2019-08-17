109b72b049860bfb3c053b9fe1606f5b0c12209b
神藤優介 <yusuke.j0728@gmail.com>
2019-08-17 11:37:30 +0900

    finish #3

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,11 +1,23 @@
 pragma solidity ^0.5.1;
 
 contract DappToken {
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0";
     uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
     // Constructor
-    constructor() public {
-        totalSupply = 1000000;
+    // constructor() public {
+    //     totalSupply = 1000000;
+    // }
+    constructor(uint256 _initialSupply) public {
+        totalSupply = _initialSupply;
+        // allocate the initial supply
+        balanceOf[msg.sender] = _initialSupply;
     }
+
     // Set the total number of tokens
     // Read the total number of tokens
 }
